f2161b8116dfad7cb147f656cc8c31d567881085
Totenfluch <github@cziegler.de>
2020-10-09 16:26:03 +0200

    enable https

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 const cors = require('cors');
 
 const CONFIG = require('./config');
@@ -12,15 +14,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(CONFIG.PORT, () => {
+/* when deploying, link the certificates with ln to the main folder */
+const privateKey = fs.readFileSync('./link_privatekey.pem');
+const certificate = fs.readFileSync('./link_certificate.pem');
+const chain = fs.readFileSync('./link_chain.pem');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: chain,
+};
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(CONFIG.PORT, () => {
     console.log(`API Server started on Port ${CONFIG.PORT}`);
 });
 
-app.get('/', (req, res) => {
+httpsServer.get('/', (req, res) => {
     res.send({ data: 'online' });
 });
 
-app.get('/api/v1/slotsData', async (req, res) => {
+httpsServer.get('/api/v1/slotsData', async (req, res) => {
     const slotsData = await slotsQueries.getSlotsData();
     res.send({ data: slotsData });
 });
